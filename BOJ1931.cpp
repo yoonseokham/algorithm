b8bdef88ce307b86669787c5287b1645a5aab4dc
ha-mulan <ham5312@naver.com>
2020-03-17 00:57:29 +0900

    greedy algorithm

--- BOJ1931.cpp
+++ BOJ1931.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int arr[100001]={0};
-int max_w=-1;
-int max_weight(int n){
-  for(int i=n-1,j=1;i>=0;i--,j++){
-    max_w=max(max_w,arr[i]*j);
+pair<int,int> arr[100001];
+bool cmp(const pair<int,int>& a,const pair<int,int>& b){
+  if(a.second<b.second)
+    return true;
+  else if(a.second==b.second){
+    if(a.first<b.first)
+      return true;
+    else
+      return false;
   }
-  return max_w;
+  else
+    return false;
+}
+int numOfConference_return(int n){
+int endTime=0;
+int numOfConference=0;
+  for(int i=0;i<n;i++){
+    if(endTime<=arr[i].first){
+      numOfConference++;
+      endTime=arr[i].second;
+    }
+  }
+  return numOfConference;
 }
 int main(void) {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   int n;
   cin>>n;
-  for(int i=0;i<n;i++)
-    cin>>arr[i];
-  sort(arr,arr+n);
-  cout<<max_weight(n);
+  for(int i=0;i<n;i++){
+    cin>>arr[i].first>>arr[i].second;
+  }
+  sort(arr,arr+n,cmp);
+  cout<<numOfConference_return(n);
   return 0;
 }
