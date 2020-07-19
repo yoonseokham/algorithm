d4488a16f34057d299f9631b589c513b30bdc302
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2020-07-20 01:09:40 +0900

    topological sort with priority queue
    topological sort doesn't need visit array

--- BOJ1766.cpp
+++ BOJ1766.cpp
@@ -5,37 +5,22 @@
 using namespace std;
 vector <int> v[MAX];
 int link[MAX]={0};
-bool visit[MAX]={false};
 priority_queue <int> q;
 int PushAndPop(int n){
   return -1*n;
 }
-bool allVisit(int n){
-  for(int i=1;i<=n;i++)
-    if(!visit[i]) return false;
-  return true;
-}//전부 방문시 트루 리턴
 void TopoloicalSort(int  n){
-    if(q.empty()&&allVisit(n))
-      return;
-    else if(q.empty()&&!allVisit(n)){
-      for(int i=1;i<=n;i++)
-        if(!visit[i]&&link[i]==0) visit[i]=true,q.push(PushAndPop(i));
-      TopoloicalSort(n);
-    }
+    if(q.empty()) return;
     else{
       int topIndx=PushAndPop(q.top());
       cout<<topIndx<<" ";
       q.pop();
       for(int i=0;i<v[topIndx].size();i++){
-        if(!visit[v[topIndx][i]]){
             link[v[topIndx][i]]--;
             if(link[v[topIndx][i]]==0){
               q.push(PushAndPop(v[topIndx][i]));
-              visit[v[topIndx][i]]=true;
             }
         }
-      }
       TopoloicalSort(n);
     }
 }
@@ -47,6 +32,8 @@ int main(void){
     v[temp1].push_back(temp2);
     link[temp2]++;
   }
+  for(int i=1;i<=n;i++)
+    if(link[i]==0) q.push(PushAndPop(i));
   TopoloicalSort(n);
   return 0;
 }
