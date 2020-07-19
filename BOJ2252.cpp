e152341bae1d25db9ca67634c7ff2c227e8f2706
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2020-07-20 00:56:11 +0900

    topological sort (no visit check)

--- BOJ2252.cpp
+++ BOJ2252.cpp
@@ -6,36 +6,19 @@ using namespace std;
 vector < int > v[MAX];
 queue <int > q;
 int link[MAX]={0};
-bool visited[MAX]={0};
-
-bool allVisit(int n){
-  for(int i=1;i<=n;i++){
-      if(!visited[i])
-        return false;
-  }
-  return true;
-}
 void TopologicalSort(int n){
-  if(q.empty()&&allVisit(n))
+  if(q.empty())//&&allVisit(n))
     return;
-  else if(q.empty()&&!allVisit(n)){
-    for(int i=1;i<=n;i++){
-      if(!visited[i]&&link[i]==0){
-        q.push(i);
-        visited[i]=true;
-      }
-    }
-    TopologicalSort(n);
-  }
   else{
     int topIndx=q.front();
     q.pop();
     cout<<topIndx<<" ";
-  for(int i=0;i<v[topIndx].size();i++){
-    if(!visited[v[topIndx][i]]&&link[v[topIndx][i]]==0)
-      q.push(v[topIndx][i]);
+    for(int i=0;i<v[topIndx].size();i++){
       link[v[topIndx][i]]--;
-    }
+      if(link[v[topIndx][i]]==0){
+        q.push(v[topIndx][i]);
+        }
+      }
     TopologicalSort(n);
   }
 }
@@ -50,8 +33,8 @@ int main(void){
   for(int i=1;i<=n;i++){
     if(link[i]==0){
       q.push(i);
-      visited[i]=true;
     }
   }
   TopologicalSort(n);
+  return 0;
 }
