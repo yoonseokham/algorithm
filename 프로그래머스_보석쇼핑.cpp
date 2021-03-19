b09a3f9c4e656eb880780c7735cedff0ad299672
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2021-03-19 12:01:08 +0900

    해당 자료가  몇개 있는지 map을 이용하여 표현

--- 프로그래머스_보석쇼핑.cpp
+++ 프로그래머스_보석쇼핑.cpp
@@ -3,24 +3,20 @@
 #include <map>
 #include <tuple>
 #include <algorithm>
-#include <iostream>
 #include <set>
 using namespace std;
-bool all_have(set<string>& gems_valid,map<string,int>& gems_count){
- return gems_valid.size()==gems_count.size();
-}
+
+bool all_have(set<string>& gems_valid,map<string,int>& gems_count){ return gems_valid.size()==gems_count.size();}
+
 vector<int> solution(vector<string> gems) {
     vector<int> answer;
     vector<tuple<int,int,int>> a;
     set<string> gems_valid;
     map<string,int> gems_count;
-    for(int i=0;i<gems.size();i++) {
-        gems_valid.insert(gems[i]);
-        // gems_count[gems[i]]=0;
-    }
     int start=0,end=0;
+    for(int i=0;i<gems.size();i++) gems_valid.insert(gems[i]);
     gems_count[gems[start]]=1;
-    while(end<gems.size()){
+    while(true){
         if(!all_have(gems_valid,gems_count)){
             end++;
             if(end==gems.size()) break;
@@ -37,6 +33,5 @@ vector<int> solution(vector<string> gems) {
     sort(a.begin(),a.end());
     answer.push_back(get<1>(a[0])+1);
     answer.push_back(get<2>(a[0])+1);
-    
     return answer;
 }
