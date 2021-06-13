eb9336486f6da83b006dc845de4bda79ba6f3ccb
ha-mulan <ham5312@naver.com>
2021-06-13 17:09:21 +0900

    greedy ***

---
+++ Baekjoon/BOJ1689.py
@@ -0,0 +1,15 @@
+import sys
+if __name__ == "__main__":
+    n=int(input())
+    cordinate=[]
+    cnt=0
+    answer=0
+    for _ in range(n):
+        start,end=map(int,sys.stdin.readline().rstrip().split())
+        cordinate.append((start,1))
+        cordinate.append((end,-1))
+    cordinate.sort()
+    for i,j in cordinate:
+        cnt+=j
+        answer=max(cnt,answer)
+    print(answer)
