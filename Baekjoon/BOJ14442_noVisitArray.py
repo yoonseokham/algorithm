260d4e878b7c65b6455863c84d826c57d53633e2
ha-mulan <ham5312@naver.com>
2021-06-23 19:15:45 +0900

    visit array에 값을 담는 대신 queue에 값을 담고 방문 체크만 해도 괜찮다.

---
+++ Baekjoon/BOJ14442_noVisitArray.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+def init(n,m,k)->list:
+    return [ [int(j) for j in sys.stdin.readline().rstrip()] for i in range(n)]
+def BFS(maze:list,k)->int:
+    q=deque([(0,0,0,1)])#현 위치,부신벽의 갯수,이동거리
+    visit=[ [ [False]*len(maze[0]) for i in range(len(maze))] for _ in range(k+1) ]
+    visit[0][0][0]=True
+    answer=1000001
+    indexInfo=list(zip([-1,0,1,0],[0,1,0,-1]))
+    while q:
+        curIndex,curJndex,curSmashed,curMoved=q.popleft()
+        if curIndex==len(maze)-1 and curJndex==len(maze[0])-1: return curMoved
+        for i,j in indexInfo:
+            newI=curIndex+i
+            newJ=curJndex+j
+            if 0<=newI<len(maze) and 0<=newJ<len(maze[0]):
+                if maze[newI][newJ]==0  and not visit[curSmashed][newI][newJ]:
+                    q.append((newI,newJ,curSmashed,curMoved+1))
+                    visit[curSmashed][newI][newJ]=True
+                if curSmashed<k and maze[newI][newJ]==1 and not visit[curSmashed+1][newI][newJ]:
+                    q.append((newI,newJ,curSmashed+1,curMoved+1))
+                    visit[curSmashed+1][newI][newJ]=True
+    return -1 if answer==1000001 else answer
+if __name__ == "__main__":
+    n,m,k=map(int,sys.stdin.readline().rstrip().split())
+    maze=init(n,m,k)
+    print(BFS(maze,k))
