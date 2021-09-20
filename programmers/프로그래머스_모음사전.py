c2e02e11895607513abee13ec7a453a5daa30797
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2021-09-20 15:36:38 +0900

    product('AEIOU',repeat=i+1)

--- programmers/프로그래머스_모음사전.py
+++ programmers/프로그래머스_모음사전.py
@@ -1,12 +1,3 @@
 from itertools import product
 def solution(word):
-    letters=[[ 'A', 'E', 'I', 'O', 'U',' '] for _ in range(5)]
-    def striper(word):
-        sum=''
-        for i in word:
-            if i!=' ':
-                sum+=i
-        if sum=='':
-            return 'A'
-        return sum
-    return {value:i for i,value in enumerate(sorted(list(set([ striper(i) for i in product(*letters) ]))))}[word]+1
+    return {value:i for i,value in enumerate(sorted([ ''.join(j) for i in range(5) for j in product('AEIOU',repeat=i+1) ]))}[word]+1
