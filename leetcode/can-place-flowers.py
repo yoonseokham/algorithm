dd45ebb8d40d5ec2908777d2ac142fc9768df8bb
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2022-05-06 00:30:17 -0700

    stack and set

---
+++ leetcode/can-place-flowers.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def inadjacentChoice(self, plantAbleSet, n):
+        plantAbles = sorted(list(plantAbleSet))
+        stack = []
+        for i, value in enumerate(plantAbles):
+            if not stack:
+                stack.append(value)
+            elif stack[-1] - value == -1:
+                continue
+            else:
+                stack.append(value)
+        return len(stack) >= n
+
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        isPlantAble = set([i for i in range(len(flowerbed))])
+        for index, planted in enumerate(flowerbed):
+            if planted:
+                for i in (-1, 0, 1):
+                    isPlantAble.discard(index - i)
+        return self.inadjacentChoice(isPlantAble, n)
